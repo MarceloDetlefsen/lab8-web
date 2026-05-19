2f2f4455fafecdd72f5a651ed3929737b5fe58ff
Marcelo Detlefsen <marcelodetlefsen1a@gmail.com>
2026-05-19 03:04:29 -0600

    Ya se muestran los componentes despues de bun run dev.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { PasswordStrengthMeter } from './components/PasswordStrengthMeter'
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1 className="app__title">Password Strength Meter</h1>
+      <PasswordStrengthMeter />
+    </main>
+  )
+}
+
+export default App
